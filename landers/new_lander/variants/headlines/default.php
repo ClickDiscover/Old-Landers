90e268a11fffd10fbd6ff9b5f778e059e366282f
Patrick Madden <maddenpj@gmail.com>
2015-08-13 22:12:27 -0500

    Add geos to variants

--- landers/new_lander/variants/headlines/default.php
+++ landers/new_lander/variants/headlines/default.php
@@ -1,2 +1,2 @@
-<h1><strong class="text-pink">Breaking:</strong> Controversial $5 'Skinny Pill' Hits The American Market.</h2>
+<h1><strong class="text-pink">Breaking:</strong> Controversial $5 'Skinny Pill' Hits The American Market.</h1>
 <h4>Think Diet Pills Don't Work? Here's One That Doctors Say May Actually Deliver.</h4>
