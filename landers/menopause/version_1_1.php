81c407a05880942d5f5f83459c22ea4f698934eb
Patrick Madden <maddenpj@gmail.com>
2015-08-20 03:38:04 -0500

    Revert menopause base.php to step 1 lander, move step 2/v1 to version_1_1.php

--- landers/menopause/version_1_1.php
+++ landers/menopause/version_1_1.php
@@ -1,6 +1,5 @@
 <?php 
 
-
 if (isset($_SERVER['HTTP_USER_AGENT']) &&
     (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false))
     header('X-UA-Compatible: IE=edge,chrome=1');
@@ -54,6 +53,9 @@ $assets .= '/';
   <!--[if lt IE 7]>
     <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
   <![endif]-->
+  <div class="facebook-exclusive">
+	<span id="bluebar">Exclusive Free Offer for  Facebook Visitors Below</span>
+</div>
   <section class="topbar">
     <div class="container">
       <div class="row">
@@ -65,9 +67,10 @@ $assets .= '/';
     <div class="container">
       <div class="row headline">
         <div class="col-sm-12">
-          <h1><strong class="text-pink"><span>BREAKING:</span></strong><b> Breakthrough Formula Has Women Dropping Weight Fast</b></h1>
-          <h4 class="text-pink"><i>New SIim-Down Helps Women Drop Weight, Stop Hot Flashes, and Get Better Sleep</i></h4>
-		  <div class="full-width-image">
+          <h1><strong class="text-pink"><span>BREAKING:</span></strong><b> Breakthrough Formula Has Women Dropping Weight Fast</b>   
+          </h1>
+          <br>
+          <div class="full-width-image">
 		  <img src="<?= $assets ?>img/img-w6.jpg">
 		  </div>
           <div class="gap20"></div>
@@ -114,7 +117,7 @@ $assets .= '/';
         <div class="col-sm-7 col-sm-pull-5">
         <div class="gap20"></div>
         <p>The product we're talking about is called <a href="<?= $step1_link ?>"><?= $step1_name ?></a>. You may have seen it in the news, on TV, or trending online as the safe and powerful "secret diet trick&rdquo; being used by models, celebrities and regular folk all across America. <a href="<?= $step1_link ?>"><?= $step1_name ?></a>'s high anti-oxidant content and powerful fat burning properties make it a belly busting machine. Combine it with the detoxifying, cleansing and additional weight loss effect of a <a href="<?= $step2_link ?>"><?= $step2_name ?></a> and you see why this deserves a Special Report. But maybe the most important part is that <?= $step1_name ?> is finally available nationwide WITHOUT a prescription.</p>
-          <p><strong>If you're interested in losing weight without diet or exercise, <a href="<?= $step1_link ?>"><?= $step1_name ?></a> is something you need to know more about.</strong></p>
+          <p><strong>If you're interested in losing weight without diet or exercise, <a href="<?= $step1_link ?>"><?= $step1_name ?></a> and <a href="<?= $step2_link ?>"><?= $step2_name ?></a> is something you need to know more about.</strong></p>
 
 
         </div>
@@ -131,12 +134,11 @@ $assets .= '/';
       <div class="row">
         <div class="col-sm-12">
           <p>&nbsp;</p>
-          <p>If you&rsquo;re feeling skeptical right now, you&rsquo;re not alone. We here at Women&rsquo;s Health have seen many promising products come and go over the last 18 years. When we first learned about &lsquo;yet another&rsquo; fat-burning/body-firming product our fad radar went off right away. Even after pouring through mountains of research. While I had an educated opinion, I still had no personal proof that <a href="<?= $step1_link ?>"><?= $step1_name ?></a> option was worth the time. So, with my editors blessing, I decided to go out and put the product to the test myself. What better way to find out the truth than to conduct my own study?</p>
+          <p>If you&rsquo;re feeling skeptical right now, you&rsquo;re not alone. We here at Women&rsquo;s Health have seen many promising products come and go over the last 18 years. When we first learned about &lsquo;yet another&rsquo; fat-burning/body-firming product our fad radar went off right away. Even after pouring through mountains of research. While I had an educated opinion, I still had no personal proof that <a href="<?= $step1_link ?>"><?= $step1_name ?></a> and <a href="<?= $step2_link ?>"><?= $step2_name ?></a> were worth the time. So, with my editors blessing, I decided to go out and put the products to the test myself. What better way to find out the truth than to conduct my own study?</p>
           <p>We know that the thought of losing your excess fat often feels like an impossible challenge. We&rsquo;ve evaluated numerous fat-loss programs and it seems like everything, too demanding or doesn&rsquo;t work as claimed. The reason why most programs fail is because they impose unrealistic restrictions on your daily life.</p>
           <p>In this special report, I explain exactly how I tested the product and show you the surprising results I achieved. Today, I&rsquo;m leaking the secret to what, we here at Women&rsquo;s Health, call &ldquo;quite possibly the most effective female physique transformation stack the world has ever seen&rdquo;. I&rsquo;ll show you how you too can get a leaner, slimmer sexier body quickly and safely without having to use illegal drugs or paying $1000&rsquo;s for surgery.</p>
           <h3><strong>The Science behind<a href="<?= $step1_link ?>"> <?= $step1_name ?></a></strong></h3>
           <p><a href="<?= $step1_link ?>"><?= $step1_name ?></a> is a small fruit which looks a bit like a tiny pumpkin. Found in Asia, Australia, Polynesia and parts of Africa, this fruit has long been used by native people because of its amazing and diversified health properties. </p>
-
           <div class="benefits">
             <h3><strong><a href="<?= $step1_link ?>"><?= $step1_name ?></a></strong> has been scientifically proven to:</h3>
             <ul>
@@ -146,16 +148,19 @@ $assets .= '/';
               <li>Increase the quality of your sleep during the night</li>
             </ul>
           </div>
-          <p>Here at Women’s Health, we were amazed to learn about how effective<a href="<?= $step1_link ?>"> <?= $step1_name ?></a> is when used for weight loss. But we noticed something else remarkable - <strong>many of the problems <a href="<?= $step1_link ?>"><?= $step1_name ?></a> treats are problems associated with menopause.</strong></p>
-          <p>Menopausal women often experience hot flashes, decreased energy levels, restlessness when sleeping and mood changes. <a href="<?= $step1_link ?>"><?= $step1_name ?></a> treats every one of these symptoms. Even better, <?= $step1_name ?> is completely natural and safe.</p> 
-          <p>If you’re a woman experiencing menopause, <a href="<?= $step1_link ?>"><?= $step1_name ?> </a>can help lessen the symptoms – and even prevent some of them from occurring in the first place.</p>
+          <div class="col-sm-5">
+         <img src="<?= $assets ?>img/beforeafterfront.jpg" class="img-responsive center-block img-thumbnail">
+         </div>
+          <p>Here at Women’s Health, we were amazed to learn about how effective<a href="<?= $step1_link ?>"> <?= $step1_name ?></a> and <a href="<?= $step2_link ?>"><?= $step2_name ?></a> were when used for weight loss. But we noticed something else remarkable - <strong>not only does <a href="<?= $step1_link ?>"><?= $step1_name ?></a> combined with <a href="<?= $step2_link ?>"><?= $step2_name ?></a> help you lose weight, these products can also help with sypmtoms associated with menopause!</strong></p>
+          <p>Menopausal women often experience hot flashes, decreased energy levels, restlessness when sleeping and mood changes. <a href="<?= $step1_link ?>"><?= $step1_name ?></a> taken with <a href="<?= $step2_link ?>"><?= $step2_name ?></a> treats every one of these symptoms. Even better, <?= $step1_name ?>  and <?= $step2_name ?> are completely natural and safe.</p> 
+          <p>If you’re a woman experiencing menopause, <a href="<?= $step1_link ?>"><?= $step1_name ?> </a> and <a href="<?= $step2_link ?>"><?= $step2_name ?></a> will help lessen the symptoms – and even prevent some of them from occurring in the first place.</p>
           <div class="benefits">
-            <h3><strong>For Women Undergoing Menopause,<a href="<?= $step1_link ?>"> <?= $step1_name ?>:</a></strong></h3>
+            <h3><strong>For Women Undergoing Menopause,<a href="<?= $step1_link ?>"> <?= $step1_name ?></a> and <a href="<?= $step2_link ?>"><?= $step2_name ?></a>:</strong></h3>
             <ul>
-              <li>Results in <strong>natural weight loss</strong> with no diet or exercise</li>
+              <li>Result in <strong>natural weight loss</strong> with no diet or exercise</li>
               <li>Reduces or even eliminates hot flashes</li>
-              <li>Improves and stabilizes mood</li>
-              <li>Improves duration and quality of sleep</li>
+              <li>Improve and stabilizes mood</li>
+              <li>Improve duration and quality of sleep</li>
             </ul>
           </div>
 
@@ -185,26 +190,26 @@ $assets .= '/';
     <div class="container">
       <div class="row">
         <div class="col-sm-12">
-          <h4><strong><a href="<?= $step1_link ?>" class="step-link"> <?= $step1_name ?></a> Clinical Studies</strong></h4>
+          <h4><strong><a href="<?= $step1_link ?>" class="step-link"> <?= $step1_name ?></a> and <a href="<?= $step2_link ?>"><?= $step2_name ?></a> Clinical Studies</strong></h4>
           <ul>
-          <li>The health journal Lipids in Health &amp; Disease  showed that subjects who took <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?> </a> lost an average of 19.3 pounds in four weeks. Subjects did not change their diet or exercise habits during the study.</li>
+          <li>The health journal Lipids in Health &amp; Disease  showed that subjects who took <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?> </a> and <a href="<?= $step2_link ?>"><?= $step2_name ?></a> lost an average of 19.3 pounds in four weeks. Subjects did not change their diet or exercise habits during the study.</li>
 
-          <li><?= $step1_name ?> contains <b>no toxins, artificial chemicals or stimulants</b>. It is 100% natural.</li>
+          <li>These products contain <b>no toxins, artificial chemicals or stimulants</b>. They are both 100% natural.</li>
 
-          <li>Studies show <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?></a> increases metabolism. In turn, the increased metabolic functions increase weight loss by more than 640%.</li>
+          <li>Studies show that <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?> </a> taken with <a href="<?= $step2_link ?>"><?= $step2_name ?></a> increases metabolism. In turn, the increased metabolic functions increase weight loss by more than 640%.</li>
 
-          <li>Studies also show a 39% <b>reduction in cholesterol</b> along with a two-inch average reduction in belly fat over four weeks.</li> 
+          <li>When <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?> </a> is taken with <a href="<?= $step2_link ?>"><?= $step2_name ?></a>, studies have also shown a 39% <b>reduction in cholesterol</b> along with a two-inch average reduction in belly fat over four weeks.</li> 
 
-          <li>Subject in the study who took <?= $step1_name ?> lost an average of 17 pounds more than study participants who were given a placebo. </li>
+          <li>Subject in the study who took <?= $step1_name ?> and <?= $step2_name ?> lost an average of 17 pounds more than study participants who were given a placebo. </li>
 
-          <li>Additional studies show that participants who took <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?> </a> had a 49% reduction in fat loss and a reduction of body weight of up to 4.6%.</li> 
+          <li>Additional studies show that participants who took <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?> </a> and <a href="<?= $step2_link ?>"><?= $step2_name ?></a> had a 49% reduction in fat loss and a reduction of body weight of up to 4.6%.</li> 
           </ul>
 
-          <p>Of course, we’d never recommend <?= $step1_name ?> without firsthand knowledge. This is why we’d like to introduce our readers to Dolores Williams.</p>
+          <p>Of course, we’d never recommend <?= $step1_name ?> and <?= $step2_name ?> without firsthand knowledge. This is why we’d like to introduce our readers to Dolores Williams.</p>
 
           <h4><strong>Dolores Williams, 52, Lost 21 Pounds in 30 Days. Here’s How She Did It and How You Can, Too!</strong></h4>
 
-          <p>When we were asking our staff if they knew any women who might be willing to test out <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?></a> for our report, almost immediately our associate editor Christina Williams raised her hand.</p>
+          <p>When we were asking our staff if they knew any women who might be willing to test out <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?></a> and <a href="<?= $step2_link ?>"><?= $step2_name ?></a> for our report, almost immediately our associate editor Christina Williams raised her hand.</p>
 
           <p>“My mother is 52 years old and just last week her doctor told she needed to lose weight as soon as possible,” said Ashley. “The problem is a heart condition that keeps her from doing any strenuous exercise. She’s been looking for a way to stay out of the gym but also drop the pounds.”</p>
           <p>Ashley’s mom, Dolores, seemed like the perfect candidate to give us an honest report of her experiences. When we approached her, she was thrilled to try it out. And, as we quickly learned, she wasn’t the type of person to hold back her opinion.</p>
@@ -213,8 +218,8 @@ $assets .= '/';
 
         <p>Hi everyone!</p>
         <p>My first step was to order the products directly from the website. With their money-back guarantee, I had nothing to worry about.</p>
-        <p>My bottle of <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?></a> arrived in four days with no problems.</p>
-        <p>Instructions were simple. I took one pill in the morning and another pill in the evening. My complete journal is below:</p> 
+        <p>My bottle of <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?></a> arrived in 2 days with no problems, and <a href="<?= $step2_link ?>"><?= $step2_name ?></a> came 2 days after that.</p>
+        <p>Instructions were simple. I took one pill of each product in the morning and evening. My complete journal is below:</p> 
 
         </div>
       </div>
@@ -226,13 +231,13 @@ $assets .= '/';
       <div class="row">
         <div class="col-sm-6">
         <div class="gap20"></div>
-          <img src="<?= $assets ?>img/img-w1.jpg" class="img-responsive center-block img-round">
         </div>
         <div class="col-sm-6">
           <h2><strong class="text-pink">Week One</strong></h2>
           <p>My doctor just told me I was fat. Well, he didn’t say it so directly – he went on about my BMI and where I fit in on the obesity scale – but I knew what he meant. I was at least 15 pounds overweight.</p>
-          <p>Going to the gym just isn’t a part of my lifestyle. I have a bad ticker and bad knees. So I needed something which would help me stop being fat, but which didn’t require diet or exercise. When my daughter suggested <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?></a>, I was intrigued.</p> 
+          <p>Going to the gym just isn’t a part of my lifestyle. I have a bad ticker and bad knees. So I needed something which would help me stop being fat, but which didn’t require diet or exercise. When my daughter suggested <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?></a> paired with <a href="<?= $step2_link ?>"><?= $step2_name ?></a>, I was intrigued.</p> 
           <p>I took one pill daily for the first week. Right away, I noticed an increase in energy. That was great, but what I really wanted was weight loss. Well, within the first week, I dropped seven pounds. But was it just water weight?</p>
+          <p><img src="<?= $assets ?>img/img-w1.jpg" class="img-responsive center-block img-round"></p>
         </div>
       </div>
     </div>
@@ -255,13 +260,12 @@ $assets .= '/';
   <section class="padding-tb-40 graybar">
     <div class="container">
       <div class="row">
-        <div class="col-sm-6">
-          <img src="<?= $assets ?>img/img-w3.jpg" class="img-responsive center-block img-round">
-        </div>
+        <div class="col-sm-6"></div>
         <div class="col-sm-6">
           <div class="gap40"></div>
           <h2><strong class="text-pink">Week Four</strong></h2>
-          <p>My doctor was completely surprised – in a good way! I lost 21 pounds in just one month. And I didn’t change my lifestyle in any way except with the addition of <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?></a>.</p> <p>In just one month, I lost enough weight that my doctor no longer considers my weight a risk factor. Here’s to the long life ahead of me!</p>
+          <p>My doctor was completely surprised – in a good way! I lost 21 pounds in just one month. And I didn’t change my lifestyle in any way except with the addition of <a href="<?= $step1_link ?>" class="step-link"><?= $step1_name ?></a> and <a href="<?= $step2_link ?>"><?= $step2_name ?></a>.</p> <p>In just one month, I lost enough weight that my doctor no longer considers my weight a risk factor. Here’s to the long life ahead of me!</p>
+          <p><img src="<?= $assets ?>img/img-w3.jpg" class="img-responsive center-block img-round"></p>
         </div>
       </div>
     </div>
@@ -276,16 +280,15 @@ $assets .= '/';
         </div>
       </div>
       <div class="row">
-        <div class="col-sm-3">
-            <img src="<?= $assets ?>img/img-c1.jpg" class="img-responsive center-block img-thumbnail"><div class="gap20"></div>
+        <div class="col-sm-3"><div class="gap20"></div>
         </div>
-        <div class="col-sm-3">
-            <img src="<?= $assets ?>img/img-c2.jpg" class="img-responsive center-block img-thumbnail"><div class="gap20"></div>
+        <div class="col-sm-3"><div class="gap20"></div>
         </div>
         <div class="col-sm-6">
-            <p><strong>The Results of Our Investigation:</strong> <a href="<?= $step1_link ?>"><?= $step1_name ?> extract</a> is an effective and safe way for women to lose weight. You’ll drop pounds quickly which, in turn, will improve all areas of your overall health. As an unexpected side effect, 
-            <?= $step1_name ?> extract is also extremely effective at managing and reducing the symptoms of menopause.</p> 
-            <p>No matter what your age is, it’s never too late to be trim and healthy – and <a href="<?= $step1_link ?>"><?= $step1_name ?> </a> is a great solution. We absolutely recommend it to our readers!</p>    
+          <p><strong>The Results of Our Investigation:</strong> <a href="<?= $step1_link ?>"><?= $step1_name ?> </a> paired with <a href="<?= $step2_link ?>"><?= $step2_name ?></a> is an effective and safe way for women to lose weight. You’ll drop pounds quickly which, in turn, will improve all areas of your overall health. As an unexpected side effect, 
+            <?= $step1_name ?> extract paired with <?= $step2_name ?> is also an extremely effective way of managing and reducing the symptoms of menopause!</p> 
+            <p>No matter what your age is, it’s never too late to be trim and healthy – and <a href="<?= $step1_link ?>"><?= $step1_name ?> </a> paired with <a href="<?= $step2_link ?>"><?= $step2_name ?></a> is a great solution. We absolutely recommend it to our readers!<span class="col-sm-3"><img src="<?= $assets ?>img/img-c1.jpg" class="img-responsive center-block img-thumbnail"></span></p>
+            <p><span class="col-sm-3"><img src="<?= $assets ?>img/img-c2.jpg" class="img-responsive center-block img-thumbnail"></span></p>    
         </div>
       </div>
     </div>
