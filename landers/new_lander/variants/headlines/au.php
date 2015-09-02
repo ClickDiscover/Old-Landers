8b98afecba902b269e0ea40c1ea20fedc1346831
Patrick Madden <maddenpj@gmail.com>
2015-09-02 00:20:16 -0500

    Remove old variants because they were only for geo

--- landers/new_lander/variants/headlines/au.php
+++
@@ -1,2 +0,0 @@
-<h1><strong class="text-pink">Breaking:</strong> Controversial $5 'Skinny Pill' Hits The Austrailian Market.</h1>
-<h4>Think Diet Pills Don't Work? Here's One That Doctors Say May Actually Deliver.</h4>
