f18c1963702c7e3537717be7f01cc4eeb8ddc50b
lxw110930 <liam.r.walter@gmail.com>
2015-09-14 19:26:44 -0500

    Create older.php

---
+++ landers/menopause/variants/headlines/older.php
@@ -0,0 +1,2 @@
+<h1><strong class="text-pink"><span>BREAKTHROUGH:</span></strong><b> Older Women are Dropping Weight after Menopause</b></h1>
+<h4 class="text-pink"><i>New Slim-Down Soothes Menopause Symptoms. Stop Hot Flashes, Sleep Better and Lose Weight Now.</i></h4>
