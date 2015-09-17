9cf9398659e7feebcf74dd806c1fd87bbf619136
lxw110930 <liam.r.walter@gmail.com>
2015-09-17 01:09:37 -0500

    Create secret5.php

---
+++ landers/menopause/variants/headlines/secret5.php
@@ -0,0 +1,2 @@
+<h1><strong class="text-pink"><span></span></strong><b> The Secret to Losing Weight After Menopause</b></h1>
+<h4 class="text-pink"><i>New Slim-Down Soothes Menopause Symptoms. Stop Hot Flashes, Sleep Better and Lose Weight Now.</i></h4>
