c7ce27df6085d050a477553b5e30ea3e2e3a4e7d
caviar89 <steveng87@gmail.com>
2015-09-17 01:17:02 -0500

    Create Terica.php

---
+++ landers/menopause/variants/headlines/Terica.php
@@ -0,0 +1,2 @@
+<h1><strong class="text-pink"><span>BREAKING:</span></strong><b> Controversial $5 Weight Loss Pill Stops Menopause</b></h1>
+<h4 class="text-pink"><i>Suffering From Menopause? We Found The Secret to Losing Weight and Stopping Hot Flashes.</i></h4>
