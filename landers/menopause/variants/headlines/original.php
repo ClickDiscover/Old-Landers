2a2f2872e20ddf7b27536842317c300c0b4d5d6f
lxw110930 <liam.r.walter@gmail.com>
2015-09-17 01:17:33 -0500

    Create original.php

---
+++ landers/menopause/variants/headlines/original.php
@@ -0,0 +1,2 @@
+<h1><strong class="text-pink"><span>BREAKING:</span></strong><b>Controversial 'Skinny Pill' Hits The American Market</b></h1>
+<h4 class="text-pink"><i>New Slim-Down Soothes Menopause Symptoms. Stop Hot Flashes, Sleep Better and Lose Weight Now.</i></h4>
