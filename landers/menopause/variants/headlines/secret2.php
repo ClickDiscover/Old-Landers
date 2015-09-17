9494c2bb2a2a1613a4d9e2e8ac7aefbec41bcdde
lxw110930 <liam.r.walter@gmail.com>
2015-09-17 00:21:37 -0500

    Create secret2.php

---
+++ landers/menopause/variants/headlines/secret2.php
@@ -0,0 +1,2 @@
+<h1><strong class="text-pink"><span></span></strong><b> The Secret to Losing 30-40 Pounds After Menopause</b></h1>
+<h4 class="text-pink"><i>New Slim-Down Soothes Menopause Symptoms. Stop Hot Flashes, Sleep Better and Lose Weight Now.</i></h4>
