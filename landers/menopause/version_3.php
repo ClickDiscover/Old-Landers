b1c0d61cd51625ac1d6dec1113e8b2191965d1fc
Gabe Rubio <gtrubio192@gmail.com>
2015-09-30 18:42:06 -0500

    bottom bar

--- landers/menopause/version_3.php
+++ landers/menopause/version_3.php
@@ -412,7 +412,7 @@ $assets .= '/';
     <div class="expire-text col-lg-6 col-md-6 col-sm-6 col-xs-6 text-right" id="counter">THIS OFFER EXPIRES IN 13 MINUTES 36 SECONDS</div>
     <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
         <a href="https://www.trysourcegarciniacambogiatrial.com/d/specialu/?AFID=170315&SID=&click_id=740219952&C1=&C2=&C3=">
-          <button style="margin-top: 15px" class="btn btn-block btn-info">Claim Yours!</button>
+          <button style="" class="btn btn-block btn-info claim-yours">Claim Yours!</button>
         </a>
     </div>
   </div>
