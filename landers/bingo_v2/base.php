42a52980a1d364a030680351cb6f512e71fa4df4
Gabe Rubio <gtrubio192@gmail.com>
2015-09-30 18:25:04 -0500

    bottom bar

--- landers/bingo_v2/base.php
+++ landers/bingo_v2/base.php
@@ -20,7 +20,7 @@
     <![endif]-->
 	<link href="<?= $assets ?>/images/css.html" rel="stylesheet" type="text/css">
 	<style>
-	body {
+	     body {
 	       font-family: 'Raleway', sans-serif;
 		   }
        @media screen and (max-width: 530px) {
@@ -880,7 +880,7 @@
        <div class="expire-textcol-lg-6 col-md-6 col-sm-6 col-xs-6 text-right" id="counter">THIS OFFER EXPIRES IN 13 MINUTES 36 SECONDS</div>
        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
            <a href="https://www.trysourcegarciniacambogiatrial.com/d/specialu/?AFID=170315&SID=&click_id=740219952&C1=&C2=&C3=">
-             <button style="margin-top: 0px" class="btn btn-info">Claim Yours!</button>
+             <button style="margin-top: 0px" class="btn btn-block btn-info">Claim Yours!</button>
            </a>
        </div>
      </div>
