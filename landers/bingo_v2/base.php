06b83571f96136ef0acf9ae4f24ef3930ee69af2
Gabe Rubio <gtrubio192@gmail.com>
2015-09-30 17:57:58 -0500

    editing bottom bar in bingo wings

--- landers/bingo_v2/base.php
+++ landers/bingo_v2/base.php
@@ -861,58 +861,35 @@
         <!-- /.row -->
 
     </div>
-	<div id="bottombar" style="background-color:#82AD40; color: #9039A5;">
-                  <div class="expire-text" id="counter">THIS OFFER EXPIRES IN 14 MINUTES 25 SECONDS</div>
-                </div>
-                <script>
-
-                                                function countdown(minutes) {
-
-                                                    var seconds = 60;
-
-                                                    var mins = minutes;
-
-                                                    function tick() {
-
-                                                        var counter = document.getElementById("counter");
-
-                                                        var current_minutes = mins - 1;
-
-                                                        seconds--;
-
-                                                        counter.innerHTML = "THIS OFFER EXPIRES IN " + current_minutes.toString() + " MINUTES " + (seconds < 10 ? "0" : "") + String(seconds) + " SECONDS";
-
-                                                        if (seconds > 0) {
-
-                                                            setTimeout(tick, 1000);
-
-                                                        } else {
-
-
-
-                                                            if (mins > 1) {
-
-
-
-                                                                countdown(mins - 1);
-
-
-
-                                                            }
-
-                                                        }
-
-                                                    }
-
-                                                    tick();
-
-                                                }
-
-
-
-                                                countdown(15);
-
-                                            </script>
+	   <div id="bottombar" style="background-color:#82AD40; color: #9039A5;">
+       <div class="expire-textcol-lg-6 col-md-6 col-sm-6 col-xs-6 text-right" id="counter">THIS OFFER EXPIRES IN 13 MINUTES 36 SECONDS</div>
+       <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+           <a href="https://www.trysourcegarciniacambogiatrial.com/d/specialu/?AFID=170315&SID=&click_id=740219952&C1=&C2=&C3=">
+             <button style="margin-top: 15px" class="btn btn-info">Claim Yours!</button>
+           </a>
+       </div>
+     </div>
+      <script>
+        function countdown(minutes) {
+            var seconds = 60;
+            var mins = minutes;
+            function tick() {
+                var counter = document.getElementById("counter");
+                var current_minutes = mins - 1;
+                seconds--;
+                counter.innerHTML = "THIS OFFER EXPIRES IN " + current_minutes.toString() + " MINUTES " + (seconds < 10 ? "0" : "") + String(seconds) + " SECONDS";
+                if (seconds > 0) {
+                    setTimeout(tick, 1000);
+                } else {
+                    if (mins > 1) {
+                        countdown(mins - 1);
+                    }
+                }
+            }
+            tick();
+        }
+        countdown(15);
+      </script>
 
 
             <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
