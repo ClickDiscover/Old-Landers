72682b305985623e2e620857b108c37d81d289c5
Gabe Rubio <gtrubio192@gmail.com>
2015-10-01 12:26:49 -0500

    bottom div

--- landers/bingo_v2/base.php
+++ landers/bingo_v2/base.php
@@ -880,10 +880,10 @@
 
     </div>
 	   <div id="bottombar" style="background-color:#82AD40; color: #9039A5;">
-       <div class="expire-text col-lg-6 col-md-6 col-sm-6 col-xs-6 text-right" id="counter">THIS OFFER EXPIRES IN 13 MINUTES 36 SECONDS</div>
+       <div class="expire-text col-lg-6 col-md-6 col-sm-6 col-xs-6 text-right" id="counter">OFFER EXPIRES IN 13 MINUTES 36 SECONDS</div>
        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
            <a href="https://www.trysourcegarciniacambogiatrial.com/d/specialu/?AFID=170315&SID=&click_id=740219952&C1=&C2=&C3=">
-             <button style="margin-top: 0px" class="btn btn-block btn-info claim-yours">Claim Yours!</button>
+             <button class="btn btn-block btn-info claim-yours">Claim Yours!</button>
            </a>
        </div>
      </div>
