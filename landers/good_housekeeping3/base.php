de0688b3a39918b27cb17e1a0a0a6707ddda1e89
Gabe <gtrubio192@gmail.com>
2015-10-16 12:50:55 -0500

    Update base.php

--- landers/good_housekeeping3/base.php
+++ landers/good_housekeeping3/base.php
@@ -24,17 +24,6 @@ function today() {
 }
 </script><style type="text/css"></style>
 
-<?php
-$step1_link ='http://google.com';
-$assets = '';
-$step1_name = 'PRODUCT1';
-$step1_image = 'http://static1.squarespace.com/static/54cfb281e4b0f7ee4c646797/55a56091e4b01f72a1ce9648/55ad8413e4b0fae1c07ca686/1437510671280/?format=1500w';
-
-$step2_link ='http://google.com';
-$step2_name = 'PRODUCT2';
-$step2_image = 'http://i00.i.aliimg.com/photo/v30/126431776_1/Gold_Standard_Sports_Nutrition_Supplement_Whey_Protein.jpg';
-?>
-
 
 
 </head>
