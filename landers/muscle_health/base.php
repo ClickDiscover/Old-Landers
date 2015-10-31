5ebc428b97d1f7f2a87bdbe4341f1f4af97385e1
Gabe Rubio <gtrubio192@gmail.com>
2015-10-31 17:21:02 -0500

    new update

--- landers/muscle_health/base.php
+++ landers/muscle_health/base.php
@@ -33,18 +33,6 @@ if (isset($_SERVER['HTTP_USER_AGENT']) &&
     <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic' rel='stylesheet' type='text/css'>
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
-    <?php
-    $step1_link ='http://google.com';
-    $assets = '';
-    $step1_name = 'PRODUCT1';
-    $step1_image = 'http://static1.squarespace.com/static/54cfb281e4b0f7ee4c646797/55a56091e4b01f72a1ce9648/55ad8413e4b0fae1c07ca686/1437510671280/?format=1500w';
-
-    $step2_link ='http://google.com';
-    $step2_name = 'PRODUCT2';
-    $step2_image = 'http://i00.i.aliimg.com/photo/v30/126431776_1/Gold_Standard_Sports_Nutrition_Supplement_Whey_Protein.jpg';
-    ?>
-
-
   </head>
   <body>
   <!--[if lt IE 7]>
