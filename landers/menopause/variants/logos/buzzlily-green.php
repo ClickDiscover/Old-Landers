7b3a4fc36a2369621818c0346037e789a85ed343
Patrick Madden <maddenpj@gmail.com>
2016-01-14 03:19:05 -0600

    Move logos to static location bc of variant template bug

--- landers/menopause/variants/logos/buzzlily-green.php
+++ landers/menopause/variants/logos/buzzlily-green.php
@@ -1,3 +1,3 @@
 <div class="col-sm-12">
-    <img src="<?= $assets ?>img/BuzzLily-logo-green.png" class="center-block img-responsive">
+    <img src="/static/logos/BuzzLily-logo-green.png" class="center-block img-responsive">
 </div>
